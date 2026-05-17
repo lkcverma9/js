bf15257bd67310370f228d6714127bbbb869a18e
lkcverma9 <saurabhvarma1993@gmail.com>
2026-05-17 07:53:11 +0000

    conversion of data type

---
+++ basic01/03_conversation.js
@@ -0,0 +1,91 @@
+// let score =33 // number
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+// let score1 ="33" // string
+
+// console.log(typeof score1)
+// console.log(typeof(score1))
+
+// let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof(valueInNumber))
+
+
+// let valueInNumber1=Number(score1)
+
+// console.log(typeof valueInNumber1)
+// console.log(typeof(valueInNumber1))
+
+// console.log("String and number to number")
+
+// let score2="33abc"
+// let valueInNumber2=Number(score2)
+
+// console.log(typeof valueInNumber2)
+// console.log(typeof(valueInNumber2))
+// console.log(valueInNumber2)// NaN== Not a Number
+
+// console.log("null to number")
+
+// let score3=null
+// let valueInNumber3=Number(score3)
+
+// console.log(typeof valueInNumber3)
+// console.log(typeof(valueInNumber3))
+// console.log(valueInNumber3)// 0
+
+
+// console.log("undefined to number")
+
+// let score4=undefined
+// let valueInNumber4=Number(score4)
+
+// console.log(typeof valueInNumber4)
+// console.log(typeof(valueInNumber4))
+// console.log(valueInNumber4)// NaN
+
+// console.log("boolean to number")
+// let score5=true
+// let valueInNumber5=Number(score5)
+
+// console.log(typeof valueInNumber5)
+// console.log(typeof(valueInNumber5))
+// console.log(valueInNumber5)// 0 for false and 1 for true
+
+// console.log("String to number")
+// let score6="da"
+// let valueInNumber6=Number(score6)
+
+// console.log(typeof valueInNumber6)
+// console.log(typeof(valueInNumber6))
+// console.log(valueInNumber6)// NaN
+
+/*
+"33aa"==>NaN 
+Type of NaN is Number
+
+*/
+
+let isLoggedIn =undefined
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+1==> true
+0,null,"",undefined== false otherwise true
+*/
+
+let some=33
+let stringSome=String(some)
+console.log(stringSome)
+console.log(typeof stringSome)
+
+
+/**
+ * conversion of data type
+ */
