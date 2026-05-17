a9d5a6601e18e0a662f1955dca84ef761f8fa391
lkcverma9 <saurabhvarma1993@gmail.com>
2026-05-17 09:13:36 +0000

    memory

--- basic01/05_datatype_summary.js
+++ basic01/05_datatype_summary.js
@@ -46,3 +46,41 @@
   console.log("symbole data type ",typeof anotherId)
     console.log("array data type ",typeof herso)
         console.log("function data type ",typeof myfunc)
+
+
+// ********************** Memory
+
+console.log("*************** Learn Memory Stack and HEAP********************");
+
+// STACK and HEAP
+
+// all primeitive data type are used stack memory 
+// add non primitive data type are used heap memory
+
+// STACK ===> Alway copy the data. so when changes copy data then its changes the copy data only
+// HEAP== always provide the refence. so when changes copy data then its changes the orginal data also
+
+    let name ="saurabh"
+    let anotherName=name
+
+    console.log(name);
+    console.log(anotherName)
+
+    anotherName="Vanya"
+    
+      console.log(name);
+    console.log(anotherName)
+
+    let userOne={
+        name :"saurabh",
+        email:"saurabh@Gmail.com"
+    }
+
+    let userTwo=userOne
+    console.log(userOne.email);
+    console.log(userTwo.email);
+    userTwo.email="Vanya@gmail.com"
+        console.log(userOne.email);
+    console.log(userTwo.email);
+    
+    
