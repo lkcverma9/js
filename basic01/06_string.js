685a5cb7bad1c7c423de2f86557c8f1d94f54492
lkcverma9 <saurabhvarma1993@gmail.com>
2026-05-17 11:01:13 +0000

    string and numb

---
+++ basic01/06_string.js
@@ -0,0 +1,17 @@
+let name ="saurabh"
+//console.log(name+"43"+" varma") // Not good way to concat
+
+// use back tick `` . String interpolcation
+//console.log(`Hello my name is ${name} and my father Name is ${'ok 1'}`); 
+
+
+let gameName =new String('Saurabh')    
+//console.log(gameName.__proto__);
+
+//console.log(gameName.toUpperCase);
+
+let userName =new String('Saurabh')   
+console.log(userName.charAt(2))
+console.log(userName.indexOf("a"))
+console.log(userName.substring(0,4))
+console.log(userName.slice(-3,4))
---
+++ basic01/07_nums_math.js
@@ -0,0 +1,33 @@
+// const score = 12
+// console.log(score);
+// console.log(typeof score);
+// const balance =new Number(100)
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString.length)
+// console.log(balance.toFixed(2))
+
+
+// const otherNumber =113.8455
+// console.log(otherNumber.toPrecision(4));
+// const hund=1000000
+// console.log(hund.toLocaleString());
+// console.log(hund.toLocaleString('en-IN'));
+
+
+// ********************************** MATH *************************
+
+// console.log(Math)
+// console.log(Math.abs(-3455))// minus value comes into positive.
+// console.log(Math.round(4.500))
+// console.log(Math.ceil(4.490))
+// console.log(Math.floor(4.490))
+// console.log(Math.max(3,2))
+console.log(Math.random())// alway comes into 0 to 1
+console.log(Math.floor(Math.random()*10)+1)
+
+const min =1000
+const max=2000
+
+console.log(Math.floor((Math.random()*(max-min+1)))+min)
